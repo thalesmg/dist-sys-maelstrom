2b2ec3ef7ace61171e1669b32d79c33da3dce32e
Thales Macedo Garitezi <thalesmg@gmail.com>
2023-03-10 21:34:41 -0300

    resilience
    had to reduce the retry interval to please maelstrom more frequently 🤔

--- src/broadcast.exs
+++ src/broadcast.exs
@@ -5,7 +5,7 @@ Mix.install([:jason])
 defmodule Broadcast do
   use GenServer
 
-  @retry_interval 1_000
+  @retry_interval 500
 
   def start_link() do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
@@ -36,40 +36,39 @@ defmodule Broadcast do
     {:noreply, state}
   end
   def handle_cast({:msg, %{"body" => %{"type" => "init"}} = msg}, state) do
-    IO.inspect(:stderr, msg, label: "got init")
+    # log(msg, "got init")
     state = %{state | node_id: msg["body"]["node_id"]}
     reply!(state.node_id, msg, %{type: "init_ok"})
     {:noreply, state}
   end
   def handle_cast({:msg, %{"body" => %{"type" => "topology"}} = msg}, state) do
-    IO.inspect(:stderr, msg, label: "got topology")
+    # log(msg, "got topology")
     state = %{state | neighbors: msg["body"]["topology"][state.node_id]}
     reply!(state.node_id, msg, %{type: "topology_ok"})
     {:noreply, state}
   end
   def handle_cast({:msg, %{"body" => %{"type" => "read"}} = msg}, state) do
-    IO.inspect(:stderr, msg, label: "got read")
+    # log(msg, "got read")
     reply!(state.node_id, msg, %{type: "read_ok", messages: :sets.to_list(state.messages)})
     {:noreply, state}
   end
   def handle_cast({:msg, %{"body" => %{"type" => "broadcast"} = body} = msg}, state) do
-    IO.inspect(:stderr, msg, label: "got broadcast")
-    broadcast_id = msg["body"]["msg_id"]
-    e = msg["body"]["message"]
+    # log(msg, "got broadcast")
+    broadcast_id = body["broadcast_id"] || body["msg_id"]
+    e = body["message"]
     state = if :sets.is_element(e, state.messages) do
       state
     else
-      Enum.reduce(state.neighbors, state, fn n, state ->
-        if n != msg["src"] do
-          send!(state.node_id, n, body)
-          pending_key = {n, broadcast_id}
-          start_retry_timer(pending_key)
-          Map.update!(state, :pending, &Map.put(&1, pending_key, body))
-        else
-          state
-        end
+      state.neighbors
+      |> Stream.filter(& &1 != msg["src"])
+      |> Enum.reduce(state, fn n, state ->
+        pbody = Map.put(body, "broadcast_id", broadcast_id)
+        send!(state.node_id, n, pbody)
+        pending_key = {n, broadcast_id}
+        start_retry_timer(pending_key)
+        Map.update!(state, :pending, &Map.put(&1, pending_key, pbody))
       end)
-      Map.update!(state, :messages, &:sets.add_element(e, &1))
+      |> Map.update!(:messages, &:sets.add_element(e, &1))
     end
     if msg["src"] in state.neighbors do
       reply!(state.node_id, msg, %{type: "broadcast_ok", broadcast_id: broadcast_id})
@@ -79,19 +78,21 @@ defmodule Broadcast do
     {:noreply, state}
   end
   def handle_cast({:msg, %{"body" => %{"type" => "broadcast_ok"}} = msg}, state) do
-    IO.inspect(:stderr, msg, label: "got broadcast_ok")
+    log(msg, "got broadcast_ok")
+    pending_key = {msg["src"], msg["body"]["broadcast_id"]}
+    state = Map.update!(state, :pending, &Map.delete(&1, pending_key))
     {:noreply, state}
   end
 
   def handle_info({:retry, pending_key = {n, _broadcast_id}}, state) do
-    IO.inspect(:stderr, pending_key, label: "gonna retry?")
-    IO.inspect(:stderr, state, label: "gonna retry? state>>")
+    log(pending_key, "gonna retry?")
+    log(state, "gonna retry? state>>")
     with {:ok, body} <- Map.fetch(state.pending, pending_key) do
-      IO.inspect(:stderr, pending_key, label: "retrying...")
+      log(pending_key, "retrying...")
       send!(state.node_id, n, body)
       start_retry_timer(pending_key)
     else
-      _ -> IO.inspect(:stderr, pending_key, label: "removed")
+      _ -> log(pending_key, "removed")
     end
     {:noreply, state}
   end
@@ -100,16 +101,17 @@ defmodule Broadcast do
     body = Map.merge(
       body,
       %{
+        "msg_id" => next_message_id(),
         in_reply_to: original_msg["body"]["msg_id"],
-        msg_id: next_message_id(),
       }
     )
     send!(from, original_msg["src"], body)
   end
 
   defp send!(from, to, body) do
+    body = Map.put(body, "msg_id", next_message_id())
     msg = Jason.encode!(%{src: from, dest: to, body: body})
-    IO.inspect(:stderr, msg, label: "gonna send")
+    log(msg, "gonna send")
     IO.puts(msg)
   end
 
@@ -127,13 +129,20 @@ defmodule Broadcast do
   defp start_retry_timer(pending_key) do
     Process.send_after(self(), {:retry, pending_key}, @retry_interval)
   end
+
+  defp log(x, label) do
+    IO.inspect(:stderr, x, label: label)
+    # _ = x
+    # _ = label
+    # nil
+  end
 end
 
 defmodule Loop do
   def loop() do
     IO.stream()
     |> Stream.each(fn l ->
-      IO.inspect(:stderr, l, label: "got")
+      # IO.inspect(:stderr, l, label: "got")
       Broadcast.raw(l)
     end)
     |> Stream.run()
@@ -141,7 +150,7 @@ defmodule Loop do
 end
 
 spawn(&Loop.loop/0)
-{:ok, broadcast} = Broadcast.start_link()
+{:ok, _broadcast} = Broadcast.start_link()
 
 receive do
   _ -> :ok
